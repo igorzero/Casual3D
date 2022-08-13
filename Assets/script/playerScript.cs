be98c677ad521f9f3cd213a9a84bd8a71aeb142f
igorzero <igor.felix2000@hotmail.com>
2022-08-13 13:01:04 -0300

    afrogames
    afrogames casual3d

--- Assets/script/playerScript.cs
+++ Assets/script/playerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class playerScript : MonoBehaviour
 {
@@ -9,22 +10,41 @@ public class playerScript : MonoBehaviour
     private float movimentoX;
     private float movimentoY;
     private float velocidade = 15.0f;
+    public Text placar;
+    private int pontos;
+
     void Start()
     {
         rbJogador = GetComponent<Rigidbody>();
-    }    
-    
-    void OnMove (InputValue valorMovimento)
+        pontos = 0;
+        ContagemDosPontos();
+    }
+
+    void ContagemDosPontos()
+    {
+        placar.text = "Pontos: " + pontos.ToString();
+    }
+
+    void OnMove(InputValue valorMovimento)
     {
         Vector2 vetorMovimento = valorMovimento.Get<Vector2>();
         movimentoX = vetorMovimento.x;
         movimentoY = vetorMovimento.y;
-    }    
+    }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         Vector3 movimento = new Vector3(movimentoX, 0.0f, movimentoY);
         rbJogador.AddForce(movimento * velocidade);
     }
-}
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("moeda"))
+        {
+            other.gameObject.SetActive(false);
+            pontos = pontos + 1;
+            ContagemDosPontos();
+        }
+    }
+}
